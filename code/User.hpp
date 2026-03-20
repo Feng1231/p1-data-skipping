63d463ee84faf1631d056bfed2a8078ee09aa961
Feng1231 <chenfeng021231@gmail.com>
2026-03-21 06:03:25 +0800

    Add mm

--- code/User.hpp
+++ code/User.hpp
@@ -6,10 +6,32 @@
 
 #include "Parameters.hpp"
 
+struct MinMax {
+    uint32_t min;
+    uint32_t max;
+};
+
 std::vector<std::byte> build_idx(std::span<const uint32_t> data, Parameters config){
-    assert(false && "Implement me!");
+    MinMax mm;
+    mm.min = UINT32_MAX;
+    mm.max = 0;
+
+    for (uint32_t v : data) {
+        if (v < mm.min) mm.min = v;
+        if (v > mm.max) mm.max = v;
+    }
+
+    std::vector<std::byte> idx(sizeof(MinMax));
+    std::memcpy(idx.data(), &mm, sizeof(MinMax));
+    return idx;
 }
 
 std::optional<size_t> query_idx(uint32_t predicate, const std::vector<std::byte>& index){
-    assert(false && "Implement me!");
+    const MinMax* mm = reinterpret_cast<const MinMax*>(index.data());
+
+    if (predicate < mm->min || predicate > mm->max) {
+        return 0;
+    }
+
+    return std::nullopt;
 }
